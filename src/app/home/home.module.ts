e75635991fcd305409d4b5c426593c0d02585d31
Said YEBBAH <syebbah@ateja.fr>
2020-04-27 11:18:22 +0200

    refact (home) : remove/clean useless code

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
+import { HomeComponent } from './home.component';
 
-import { HomePageRoutingModule } from './home-routing.module';
+import { HomeRoutingModule } from './home-routing.module';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomeRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomeComponent]
 })
-export class HomePageModule {}
+export class HomeModule {}
