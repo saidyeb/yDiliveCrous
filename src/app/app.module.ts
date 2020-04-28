4595183308f6546a96a019dc8ebc60104dd74615
Said YEBBAH <syebbah@ateja.fr>
2020-04-28 14:08:04 +0200

    refact (app) : add frence format as default format for date, devise, ect... :ok_hand:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -9,6 +9,11 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+//France format date, devise...
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+registerLocaleData(localeFr);
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,7 +21,11 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, 
+    {
+      provide: LOCALE_ID,
+      useValue: 'fr-FR' // for France ...
+    }
   ],
   bootstrap: [AppComponent]
 })
