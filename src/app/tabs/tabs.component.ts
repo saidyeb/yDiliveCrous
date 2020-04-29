bcde43914cdf8708eabc8a6015822bd5a1e8aa18
Said YEBBAH <syebbah@ateja.fr>
2020-04-30 01:11:18 +0200

    refact (tabs) : add panel redirection and display back button whenever the panel page is open

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -14,8 +14,8 @@ export class TabsComponent implements OnInit {
   ngOnInit() {}
 
 
-  public isDetailRoute() : boolean {
-    const routeMatch = this.router.url.match("details/([0-9]+)$"); 
+  public isDisplayBackButton() : boolean {
+    const routeMatch = this.router.url.match("details/([0-9]+)$") || this.router.url.match("/panel$"); 
     if(routeMatch != null && routeMatch.length > 0)
       return routeMatch[0].length > 0
 
